077bbaf9e7fcac12c7d6d225218d479d55d14cce
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-08-18 10:42:35 -1000

    morning challenge completed

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,31 +5,39 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
+function _sumFibs(maxFibValue) {
   var sum = 0;
-
-  // do your work here
+  let arr = [1, 2];
+  let i = 0;
+  while (arr[i] + arr[i + 1] <= maxFibValue) {
+    // arr[i] = arr[i - 1] + arr[i - 2];
+    arr.push(arr[i] + arr[i + 1]);
+    i++;
+  }
+  sum = arr.filter(elem => elem % 2 === 0).reduce((acc, sum) => {
+    return acc + sum;
+  });
 
   return sum;
 }
+// console.log(_sumFibs(10));
 
 // bonus round
-function _highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber(maxFibValue) {
   var highest = 0;
 
   //define your base case, validate your input
 
-
   //do your work here
 
   return highest;
-};
+}
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
